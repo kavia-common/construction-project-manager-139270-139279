6c5776599193406d5b746e75129122e9df0138a0
Kavia AI <production@kavia.ai>
2025-09-22 09:59:10 +0000

    CheckPoint - cge04cd82a

--- construction_frontend/src/components/figmaScreens/NarrowAds.jsx
+++ construction_frontend/src/components/figmaScreens/NarrowAds.jsx
@@ -28,11 +28,11 @@ const NarrowAds = () => {
           {/* Add to cart small narrow (14:15) */}
           <div id="comp-14-15" className="component abs" style={{ backgroundColor: 'transparent' }}>
             <div className="rect-8-3 abs style-1" />
-            <div className="text-8-4 abs text-typo-23">Product type</div>
-            <div className="text-8-5 abs text-typo-22">Name, size/weight, materials</div>
-            <div className="price-8-6 abs text-typo-24">$00.00</div>
-            <div className="frame-8-10 abs frame-center style-2">
-              <div className="label-8-8 button-label text-typo-25">Add to cart</div>
+            <div className="text-8-4 abs typo-23">Product type</div>
+            <div className="text-8-5 abs typo-22">Name, size/weight, materials</div>
+            <div className="price-8-6 abs typo-24">$00.00</div>
+            <div className="frame-8-10 abs frame-center style-2 ds-radius-5">
+              <div className="label-8-8 button-label typo-25">Add to cart</div>
             </div>
             <div className="img-8-11 abs image" style={{ backgroundImage: 'url(/assets/figma_image_8_11.png)' }} />
           </div>
@@ -40,11 +40,11 @@ const NarrowAds = () => {
           {/* Choose options small narrow (14:16) */}
           <div id="comp-14-16" className="component abs">
             <div className="rect-8-12 abs style-1" />
-            <div className="text-8-13 abs text-typo-23">Product type</div>
-            <div className="text-8-14 abs text-typo-22">Name, materials</div>
-            <div className="price-8-15 abs text-typo-24">$00.00</div>
-            <div className="frame-8-16 abs frame-center style-2">
-              <div className="label-8-17 button-label text-typo-25">Choose options</div>
+            <div className="text-8-13 abs typo-23">Product type</div>
+            <div className="text-8-14 abs typo-22">Name, materials</div>
+            <div className="price-8-15 abs typo-24">$00.00</div>
+            <div className="frame-8-16 abs frame-center style-2 ds-radius-5">
+              <div className="label-8-17 button-label typo-25">Choose options</div>
             </div>
             <div className="img-8-18 abs image" style={{ backgroundImage: 'url(/assets/figma_image_8_18.png)' }} />
           </div>
@@ -52,11 +52,11 @@ const NarrowAds = () => {
           {/* Sold out small narrow (14:17) */}
           <div id="comp-14-17" className="component abs">
             <div className="rect-8-19 abs style-1" />
-            <div className="text-8-20 abs text-typo-23">Product type</div>
-            <div className="text-8-21 abs text-typo-22">Name, size/weight, materials</div>
-            <div className="price-8-22 abs text-typo-24">$00.00</div>
-            <div className="frame-8-23 abs frame-center style-4">
-              <div className="label-8-24 button-label text-typo-25">Sold Out</div>
+            <div className="text-8-20 abs typo-23">Product type</div>
+            <div className="text-8-21 abs typo-22">Name, size/weight, materials</div>
+            <div className="price-8-22 abs typo-24">$00.00</div>
+            <div className="frame-8-23 abs frame-center style-4 ds-radius-5">
+              <div className="label-8-24 button-label typo-25">Sold Out</div>
             </div>
             <div className="img-8-25 abs image" style={{ backgroundImage: 'url(/assets/figma_image_8_25.png)' }} />
           </div>
@@ -64,11 +64,11 @@ const NarrowAds = () => {
           {/* Add to Cart tall narrow (14:18) */}
           <div id="comp-14-18" className="component abs">
             <div className="rect-13-2 abs style-1" />
-            <div className="text-13-3 abs text-typo-23">Product type</div>
-            <div className="text-13-4 abs text-typo-22">Name, size/weight, materials</div>
-            <div className="price-13-5 abs text-typo-24">$00.00</div>
-            <div className="frame-13-6 abs frame-center style-2">
-              <div className="label-13-7 button-label text-typo-25">Add to cart</div>
+            <div className="text-13-3 abs typo-23">Product type</div>
+            <div className="text-13-4 abs typo-22">Name, size/weight, materials</div>
+            <div className="price-13-5 abs typo-24">$00.00</div>
+            <div className="frame-13-6 abs frame-center style-2 ds-radius-5">
+              <div className="label-13-7 button-label typo-25">Add to cart</div>
             </div>
             <div className="img-13-8 abs image" style={{ backgroundImage: 'url(/assets/figma_image_13_8.png)' }} />
           </div>
@@ -76,11 +76,11 @@ const NarrowAds = () => {
           {/* Choose Options tall narrow (14:19) */}
           <div id="comp-14-19" className="component abs">
             <div className="rect-13-9 abs style-1" />
-            <div className="text-13-10 abs text-typo-23">Product type</div>
-            <div className="text-13-11 abs text-typo-22">Name, materials</div>
-            <div className="price-13-12 abs text-typo-24">$00.00</div>
-            <div className="frame-13-13 abs frame-center style-2">
-              <div className="label-13-14 button-label text-typo-25">Choose options</div>
+            <div className="text-13-10 abs typo-23">Product type</div>
+            <div className="text-13-11 abs typo-22">Name, materials</div>
+            <div className="price-13-12 abs typo-24">$00.00</div>
+            <div className="frame-13-13 abs frame-center style-2 ds-radius-5">
+              <div className="label-13-14 button-label typo-25">Choose options</div>
             </div>
             <div className="img-13-15 abs image" style={{ backgroundImage: 'url(/assets/figma_image_13_15.png)' }} />
           </div>
@@ -88,11 +88,11 @@ const NarrowAds = () => {
           {/* Sold Out tall narrow (14:20) */}
           <div id="comp-14-20" className="component abs">
             <div className="rect-13-16 abs style-1" />
-            <div className="text-13-17 abs text-typo-23">Product type</div>
-            <div className="text-13-18 abs text-typo-22">Name, size/weight, materials</div>
-            <div className="price-13-19 abs text-typo-24">$00.00</div>
-            <div className="frame-13-20 abs frame-center style-4">
-              <div className="label-13-21 button-label text-typo-25">Sold Out</div>
+            <div className="text-13-17 abs typo-23">Product type</div>
+            <div className="text-13-18 abs typo-22">Name, size/weight, materials</div>
+            <div className="price-13-19 abs typo-24">$00.00</div>
+            <div className="frame-13-20 abs frame-center style-4 ds-radius-5">
+              <div className="label-13-21 button-label typo-25">Sold Out</div>
             </div>
             <div className="img-13-22 abs image" style={{ backgroundImage: 'url(/assets/figma_image_13_22.png)' }} />
           </div>
